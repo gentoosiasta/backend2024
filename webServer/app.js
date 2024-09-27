2676bfdc3c2fbe82581e750d70c5051f1a1a493f
Julio Aguilar Carmona <julio.aguilarc@gmail.com>
2024-09-27 11:12:13 -0600

    Agrega el proyecto webserver

---
+++ webServer/app.js
@@ -0,0 +1,11 @@
+const { createServer } = require("http");
+
+const server = createServer((req, res) => {
+  console.log(req.url);
+  res.writeHead(500);
+  res.write("Bienvenidos a mi primer servidor web");
+  res.end();
+});
+
+server.listen(8080);
+console.log("Servidor web iniciado en 8080");
